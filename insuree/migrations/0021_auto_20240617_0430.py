adc069056e7410bce5f2da08591b40c7b10992fe
maxime ngoe <maxime.ngoe@gmail.com>
2024-06-17 05:10:02 +0100

    ajustement migration

--- insuree/migrations/0021_auto_20240617_0430.py
+++ insuree/migrations/0021_auto_20240617_0430.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('insuree', '0019_auto_20231026_1205'),
+        ('insuree', '0020_auto_20240205_0934'),
     ]
 
     operations = [
