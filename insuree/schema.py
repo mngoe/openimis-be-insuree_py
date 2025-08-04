c7a9028e598b4e55e05c6806a07c31e66ea157a3
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-08-04 12:04:01 +0100

    Fix: Remove select_related from Insuree query due to conflicts with GraphQL reports

--- insuree/schema.py
+++ insuree/schema.py
@@ -196,8 +196,8 @@ class Query(ExportableQueryMixin, graphene.ObjectType):
             filters += [Q(LocationManager().build_user_location_filter_query(info.context.user._u, prefix='current_village__parent__parent', loc_types=['D']) |
                         LocationManager().build_user_location_filter_query(info.context.user._u, prefix='family__location__parent__parent', loc_types=['D']))]
 
-        # return gql_optimizer.query(Insuree.objects.filter(*filters).all(), info)
-        return gql_optimizer.query(Insuree.objects.select_related('family', 'gender', 'health_facility', 'current_village').filter(*filters).all(), info)
+        return gql_optimizer.query(Insuree.objects.filter(*filters).all(), info)
+        # return gql_optimizer.query(Insuree.objects.select_related('family', 'gender', 'health_facility', 'current_village').filter(*filters).all(), info)
 
     def resolve_family_members(self, info, **kwargs):
         if not info.context.user.has_perms(InsureeConfig.gql_query_insuree_family_members):
