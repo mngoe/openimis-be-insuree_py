88e8981174586cdc5241e3e2ed5e47b4acc49800
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-08-20 12:56:07 +0100

    Update 0029_auto_20230923_2238.py

--- insuree/migrations/0029_auto_20230923_2238.py
+++ insuree/migrations/0029_auto_20230923_2238.py
@@ -13,12 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        
-        migrations.AddField(
-            model_name='family',
-            name='json_ext',
-            field=models.JSONField(blank=True, db_column='JsonExt', null=True),
-        ),
         migrations.AddField(
             model_name='insuree',
             name='json_ext',
