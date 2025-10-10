48cd020287e94ebda19772b99f98a970a26d9991
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-10-10 08:08:50 +0100

    fix unit tests

--- insuree/tests/test_graphql.py
+++ insuree/tests/test_graphql.py
@@ -31,13 +31,18 @@ class InsureeGQLTestCase(openIMISGraphQLTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.test_village = create_test_village()
-        cls.test_insuree = create_test_insuree(with_family=True, is_head=True, custom_props={'current_village':cls.test_village}, family_custom_props={'location':cls.test_village})
+        cls.test_insuree = create_test_insuree(
+          with_family=True,
+          is_head=True,
+          custom_props={'current_village':cls.test_village},
+          family_custom_props={'location':cls.test_village}
+        )
         cls.admin_user = create_test_interactive_user(username="testLocationAdmin")
         cls.admin_token = get_token(cls.admin_user, DummyContext(user=cls.admin_user))
         cls.ca_user = create_test_interactive_user(username="testLocationNoRight", roles=[9])
         cls.ca_token = get_token(cls.ca_user, DummyContext(user=cls.ca_user))
         cls.admin_dist_user = create_test_interactive_user(username="testLocationDist")
-        assign_user_districts(cls.admin_dist_user, ["1", "01SOA", "2", "02ABO", cls.test_village.parent.parent.code])
+        assign_user_districts(cls.admin_dist_user, [cls.test_village.parent.parent.code])
         cls.admin_dist_token = get_token(cls.admin_dist_user, DummyContext(user=cls.admin_dist_user))
         cls.photo_base64 = "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAAA1BMVEW10NBjBBbqAAAAH0lEQVRoge3BAQ0AAADCoPdPbQ43oAAAAAAAAAAAvg0hAAABmmDh1QAAAABJRU5ErkJggg=="
 
